f8903dd1cef1ff5b43d665965d86f6c301a0ad43
g-sano <g.sano.2441@gmail.com>
2020-12-07 22:27:06 +0900

    ルーターを整理

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import {
   StylesProvider,
 } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import Page from 'components/05_pages/PaymentPage'
 import { theme } from 'constants/globalUITheme'
-import Routor from 'router'
 import { store } from 'stores/configureStore'
 
 ReactDOM.render(
@@ -18,7 +18,7 @@ ReactDOM.render(
         <MaterialThemeProvider theme={theme}>
           <StyledThemeProvider theme={theme}>
             <CssBaseline />
-            <Routor />
+            <Page />
           </StyledThemeProvider>
         </MaterialThemeProvider>
       </StylesProvider>
