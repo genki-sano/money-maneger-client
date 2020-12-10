d2da724d5d8b4493f81d9d2fbf4ead38e25a46c1
g-sano <g.sano.2441@gmail.com>
2020-12-10 21:30:55 +0900

    APIのレスポンス形式変更に伴う変更

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -1,3 +1,6 @@
+import moment from 'moment'
+import { groupBy } from 'utils'
+
 export interface Total {
   women: number
   men: number
@@ -19,15 +22,19 @@ interface PaymentResponse {
 
 export const fetchPayments = async (date: string): Promise<PaymentResponse> => {
   const params = {
-    uri: '/api/list',
     date: date,
   }
-  const queryParams = new URLSearchParams(params)
   const uri = process.env.REACT_APP_URI || 'http://localhost::8080'
+  const queryParams = new URLSearchParams(params)
   const res = await fetch(`${uri}?${queryParams}`)
   const json = await res.json()
   if (!res.ok) {
     throw new Error(json.message)
   }
-  return json
+  return {
+    items: groupBy(json.items, (cur: Payment) =>
+      moment(cur.date).format('YYYY/MM/DD'),
+    ),
+    total: json.total,
+  }
 }
